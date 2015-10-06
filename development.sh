16380df8d76f628f70e5cfb194850c589da9a8be
Plechi <github@plechinger.at>
2015-10-06 12:41:46 +0200

    comment scripts, rename dist script

--- development.sh
+++ development.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+
+#this script starts the grunt watch process and a simple python webserver.
+
 grunt watch & python -m SimpleHTTPServer
--- dist.sh
+++ dist.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+
+#script to make folder for deployment (copy necessary files to the folder "dist")
+
 rm -R ./dist; mkdir dist
 cp index.html normalize.css table.css table.html turingmachine.css turingmachine.js ./dist/
 cp -R ./markets ./dist/markets
